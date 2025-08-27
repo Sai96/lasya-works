cf8b4555c2b09da4a2c7e3f8c34aa8873aa502fb
Manikanta <manikantajayanthi26@gmail.com>
2025-08-27 18:21:33 +0530

    basic changes in navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('lasya-works');
+  protected readonly title = signal('Everyone loves Lasya! 🎉');
 }
